8a641ecbeff51b0bcfa6ea7bc401764e392ee95a
NicolasCardenas <nicolas.cardenas.aroca@gmail.com>
2024-02-19 20:48:05 -0300

    "Entrega de proyecto"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
-// Tooltip boton Enviar
+    
+// navbar scroll "Al descender de la altura del carrusel el navbar tiene fondo negro"
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('nav');
+    if (window.scrollY > document.querySelector('#carouselAutoplaying').offsetHeight) {
+        navbar.classList.add('navbar-black-bg');
+    } else {
+        navbar.classList.remove('navbar-black-bg');
+    }
+});
+
+// Tooltip boton Enviar "al pasar el cursor sobre el botón enviar este despliega un tooltip"
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// Alerta de enviado
+// Alerta de enviado "al presionar el botón enviar del form, este arroja una alerta"
 const enviarBtn = document.querySelectorAll('[data-bs-title="Contacta con nosotros"]');
 
 enviarBtn.forEach(btn => {
@@ -16,15 +27,6 @@ const mensaje = "Gracias por contactar con nosotros";
 alert(mensaje);
 }
 
-    // navbar scroll
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('nav');
-    if (window.scrollY > document.querySelector('#carouselAutoplaying').offsetHeight) {
-        navbar.classList.add('navbar-black-bg');
-    } else {
-        navbar.classList.remove('navbar-black-bg');
-    }
-});
 
 
 
